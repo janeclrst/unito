80734a4cb39847a64105c88a9589c111c973f7b1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-12-25 08:55:41 +0700

    refactor: use class in onboard title

--- lib/feat_onboarding/view_models/contents_data.dart
+++ lib/feat_onboarding/view_models/contents_data.dart
@@ -5,22 +5,22 @@ import 'package:unito/feat_onboarding/models/onboard.dart';
 List<Onboard> contentsData = [
   Onboard(
       imgPath: Assets.illustrations.onboarding.illustrations0.path,
-      title: title1,
-      subTitle: subTitle1),
+      title: OnboardTitle.title1,
+      subTitle: OnboardTitle.subTitle1),
   Onboard(
       imgPath: Assets.illustrations.onboarding.illustrations1.path,
-      title: title2,
-      subTitle: subTitle2),
+      title: OnboardTitle.title2,
+      subTitle: OnboardTitle.subTitle2),
   Onboard(
       imgPath: Assets.illustrations.onboarding.illustrations2.path,
-      title: title3,
-      subTitle: subTitle3),
+      title: OnboardTitle.title3,
+      subTitle: OnboardTitle.subTitle3),
   Onboard(
       imgPath: Assets.illustrations.onboarding.illustrations3.path,
-      title: title4,
-      subTitle: subTitle4),
+      title: OnboardTitle.title4,
+      subTitle: OnboardTitle.subTitle4),
   Onboard(
       imgPath: Assets.illustrations.onboarding.illustrations4.path,
-      title: title5,
-      subTitle: subTitle5),
+      title: OnboardTitle.title5,
+      subTitle: OnboardTitle.subTitle5),
 ];
