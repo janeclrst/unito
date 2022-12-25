06148340af3de9c86a467ff07c88156381f4b0a1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-12-25 07:05:23 +0700

    chore: comment builder resonsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:responsive_framework/responsive_framework.dart';
+// import 'package:responsive_framework/responsive_framework.dart';
 import 'package:unito/screen/onboarding/onboarding_screen.dart';
 
 void main() {
@@ -19,18 +19,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: const Color(0x06000AFF)),
       home: const OnboardingScreen(),
-      builder: (context, child) => ResponsiveWrapper.builder(
-        child,
-        maxWidth: 1200,
-        minWidth: 480,
-        defaultScale: true,
-        breakpoints: const [
-          ResponsiveBreakpoint.resize(480, name: MOBILE),
-          ResponsiveBreakpoint.autoScale(800, name: TABLET),
-          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-          ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-        ],
-      ),
+      // builder: (context, child) => ResponsiveWrapper.builder(
+      //   child,
+      //   maxWidth: 1200,
+      //   minWidth: 480,
+      //   defaultScale: true,
+      //   breakpoints: const [
+      //     ResponsiveBreakpoint.resize(480, name: MOBILE),
+      //     ResponsiveBreakpoint.autoScale(800, name: TABLET),
+      //     ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+      //     ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+      //   ],
+      // ),
     );
   }
 }
