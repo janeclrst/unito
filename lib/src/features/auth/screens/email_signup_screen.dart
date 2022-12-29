6ee171dd507699c1aed6cf4c84ee45ca260c8e63
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-12-29 16:23:01 +0700

    feat: add email sign up screen

---
+++ lib/src/features/auth/screens/email_signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmailSignUp extends StatefulWidget {
+  EmailSignUp({Key? key}) : super(key: key);
+
+  @override
+  State<EmailSignUp> createState() => _EmailSignUpState();
+}
+
+class _EmailSignUpState extends State<EmailSignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
