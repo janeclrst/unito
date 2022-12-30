59588f1f0fe814e9d72520e199c208406686e734
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-12-30 16:51:57 +0700

    feat: disabled button when checkbox isn't checked

--- lib/src/features/auth/buttons/long_primary_button.dart
+++ lib/src/features/auth/buttons/long_primary_button.dart
@@ -7,25 +7,35 @@ class LongPrimaryButton extends StatelessWidget {
     Key? key,
     required this.text,
     required this.navTo,
+    required this.checkBoxValue,
   }) : super(key: key);
   final String text;
   final Widget navTo;
+  final bool checkBoxValue;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => navTo));
-      },
+      onPressed: checkBoxValue
+          ? () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => navTo,
+                ),
+              );
+            }
+          : null,
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8.0),
           ),
         ),
-        backgroundColor: MaterialStateProperty.all<Color>(
-          ExtendedColors.violet800,
-        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          return checkBoxValue ? ExtendedColors.violet800 : Colors.grey;
+        }),
         minimumSize: MaterialStateProperty.all<Size>(
           Size(MediaQuery.of(context).size.width, 48.0),
         ),
